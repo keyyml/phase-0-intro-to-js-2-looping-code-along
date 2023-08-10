9799c8d48d97cfb9b3fac273943d4a0254550b45
keyla <keyyleiva@gmail.com>
2023-08-09 23:11:20 -0400

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = [ "Guadalupe", "Ollie","Aki"]
+const events = "surprise"
+const newArray = []
+function writeCards(names, events){
+     for (let i = 0; i < names.length; i++){
+     const messages = (`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+     newArray.push(messages)
+     }
+     
+     return newArray
+
+    }
+
+function countDown(){
+    for (let countDown = 10; countDown < 11; countDown --){
+        if (countDown === -1 ) {break;}
+        console.log(countDown)
+    }
+}
